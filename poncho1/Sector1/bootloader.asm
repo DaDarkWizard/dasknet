9e8f76e01a95770df6f40fc190356797a35000fc
DaDarkWizard <46545299+DaDarkWizard@users.noreply.github.com>
2022-11-30 20:20:18 -0500

    Update bootloader.asm
    Commented my code

--- poncho1/Sector1/bootloader.asm
+++ poncho1/Sector1/bootloader.asm
@@ -2,12 +2,12 @@
 
 mov [BOOT_DISK], dl
 
-mov bp, 0x7c00
+mov bp, 0x7c00 ; Setup the stack
 mov sp, bp
 
-call ReadDisk
+call ReadDisk ; Load the rest of the OS from the disk
 
-jmp PROGRAM_SPACE
+jmp PROGRAM_SPACE ; Jump to the main part of the bootloader
 
 %include "print.asm"
 %include "DiskRead.asm"
